b3150c4d0bc09401e8e18ae366e4aaa8dcffdad9
Asmei <kamsykula@gmail.com>
2020-06-08 17:34:17 +0200

    prepare mocks for coffee machine dependencies

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -2,14 +2,29 @@ package edu.iis.mto.testreactor.coffee;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CoffeeMachineTest {
-
-    @Test
-    public void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
-    }
-
+	
+	@Mock
+	private CoffeeReceipes receipes;
+	@Mock
+	private Grinder grinder;
+	@Mock
+	private MilkProvider milkProvider;
+	private CoffeeMachine coffeeMachine;
+	
+	@BeforeEach
+	void before() {
+		coffeeMachine = new CoffeeMachine(grinder, milkProvider, receipes);
+	}
+	
+	
 }
